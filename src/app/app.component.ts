4fe67645e8dbdd242229b6d09a5ed453ece73732
Osvaldofrancisco-oc <61027333+Osvaldofrancisco-oc@users.noreply.github.com>
2025-07-05 22:22:49 +0100

    subindo_projecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { CarroselComponent } from "./carrosel/carrosel.component";
+import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
+import { GaleriaComponent } from "./galeria/galeria.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, CarroselComponent, SobreNosComponent, GaleriaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
---
+++ src/app/carrosel/carrosel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrosel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carrosel.component.html',
+  styleUrl: './carrosel.component.less'
+})
+export class CarroselComponent {
+
+}
