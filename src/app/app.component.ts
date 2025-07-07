a0d6c3f0712c23a226dcfa0ccce60492c1092b7c
Osvaldofrancisco-oc <61027333+Osvaldofrancisco-oc@users.noreply.github.com>
2025-07-07 11:53:52 +0100

    modal-1.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { CarroselComponent } from "./carrosel/carrosel.component";
 import { SobreNosComponent } from "./sobre-nos/sobre-nos.component";
 import { GaleriaComponent } from "./galeria/galeria.component";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, CarroselComponent, SobreNosComponent, GaleriaComponent],
+  imports: [CommonModule,NgbModule, NavbarComponent, CarroselComponent, SobreNosComponent, GaleriaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, TemplateRef } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalModule, BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
+  providers:[BsModalService],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.less'
 })
 export class GaleriaComponent {
+
+
+    modalRef?: BsModalRef;
+
+
+  imagens: string[] = [
+    '../../assets/img/img/1.jpg',
+    '../../assets/img/img/2.jpg',
+    '../../assets/img/img/3.jpg',
+    '../../assets/img/img/4.jpg',
+    '../../assets/img/img/5.jpg',
+    '../../assets/img/img/6.jpg',
+    '../../assets/img/img/7.jpg',
+    '../../assets/img/img/8.jpg',
+    '../../assets/img/img/9.jpg',
+    '../../assets/img/img/10.jpg',
+    '../../assets/img/img/11.jpg',
+    '../../assets/img/img/12.jpg',
+    '../../assets/img/img/13.jpg',
+    '../../assets/img/img/14.jpg',
+    '../../assets/img/img/15.jpg',
+  ]
+
+imagemSelecionada: string = '';
+
+constructor(private modalService: NgbModal,private modalService1: BsModalService){
+
+}
+
+  openModal(template: TemplateRef<any>, imagem:string) {
+    this.imagemSelecionada = imagem
+    this.modalRef = this.modalService1.show(template);
+  }
+
+
+abrirModal(conteudoModal:any, imagem:string){
+  this.imagemSelecionada = imagem;
+  this.modalService.open(conteudoModal, {
+    centered:true,
+    backdrop:true,
+    keyboard:true
+  })
+}
 
 }
