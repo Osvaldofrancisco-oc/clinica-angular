8ae3f99381bebcf9c126141747406c90a735e031
Osvaldofrancisco-oc <61027333+Osvaldofrancisco-oc@users.noreply.github.com>
2025-07-08 01:13:40 +0100

    melhorias e footer add

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CarroselComponent } from "../carrosel/carrosel.component";
+import { SobreNosComponent } from "../sobre-nos/sobre-nos.component";
+import { GaleriaComponent } from "../galeria/galeria.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CarroselComponent, SobreNosComponent, GaleriaComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.less'
 })
