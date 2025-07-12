03b7a78278ba1e820d66a4441af40bfba27cbd75
Osvaldofrancisco-oc <61027333+Osvaldofrancisco-oc@users.noreply.github.com>
2025-07-12 15:12:05 +0100

    removendo ng-bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from "./componentes/navbar/navbar.component";
 import { RouterModule } from '@angular/router'; // ✅ IMPORTANTE
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 import { FooterComponent } from "./componentes/footer/footer.component";
 
 @Component({
@@ -10,7 +10,7 @@ import { FooterComponent } from "./componentes/footer/footer.component";
     standalone:true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less'],
-    imports: [NgbModule,RouterModule,CommonModule,NavbarComponent,FooterComponent]
+    imports: [RouterModule,CommonModule,NavbarComponent,FooterComponent]
 })
 export class AppComponent {
   title = 'TestAngular';
